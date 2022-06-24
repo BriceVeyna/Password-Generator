f95ff5b8da8fdb979ca68f298eb945d308195a58
Brice Veyna <bricev99@gmail.com>
2022-06-24 06:41:09 -0700

    Fill password text arrays

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // Assignment code here
 var passwordText = {
-  lowerCase: [];
-  upperCase: [];
-  numeric: [];
-  specialCharacter: [];
+  lowerCase: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  upperCase: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  numeric: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  specialCharacter: [" ", "!", '"', "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
 }
 
 // Get references to the #generate element
